5c50c8810c48d1a23d5c45276bb0787c200d661c
Evan <evan@42maru.ai>
2019-10-08 15:54:29 +0900

    Working code for files where line number is continues, discontinued only for tables

--- line_number_cleaner_v1.py
+++ line_number_cleaner_v1.py
@@ -29,15 +29,27 @@ with open(FILE_NAME) as f:
                 # count the leading spaces in a string
                 line_leading_spaces = len(line_without_number) - len(line_without_number.lstrip(' '))
                 if line_without_number.strip():
-                    print(f"line: {line} spaces = {line_leading_spaces}")
+                    #                     print(f"line: {line} spaces = {line_leading_spaces}")
                     break
 
-print(line_leading_spaces)
+# print(line_leading_spaces)
 text = ''
+
 with open(FILE_NAME) as f:
     char_num = 0
-    for line in f:
-        if line.strip() and (line[0].isdigit() or line[1].isdigit() or line[2].isdigit()) and line.find('......') == -1:
+    count = 0
+    lst_line = []
+    for i, line in enumerate(f):
+
+        #         if i > 649:
+        #             count += 1
+        #             lst_line.append(line)
+        #         if count == 15:
+        #             break
+
+        # Check if the line is not empty and first or second characters are digits
+        # And line doesn't belong to a table of contents
+        if line.strip() and (line[0].isdigit() or line[1].isdigit()) and line.find('......') == -1:
             # Found a line with the number at the beginning
             if line.split()[0].isdigit():
                 # Slice the line so the sentence starts after the number
@@ -49,14 +61,13 @@ with open(FILE_NAME) as f:
                 # print(line_without_number)
                 text += line_without_number
 
-        # Line without a number
         else:
             # Found the end of a page
             if line[0] == '\x0c':
                 # print(NEW_PAGE)
                 line = line[1:]
                 text += NEW_PAGE
-
+                # Find leading spaces in a leading
             if len(line) - len(line.lstrip(' ')) >= 1:
                 # print(line[line_leading_spaces + char_num:])
                 text += line[line_leading_spaces + char_num:]
@@ -64,34 +75,11 @@ with open(FILE_NAME) as f:
                 # print(line)
                 text += line
 
+# print(lst_line)
 print(text)
 
-#                 if line_leading_spaces != 0:
-#                     if line_leading_spaces in dict_of_spaces:
-#                         dict_of_spaces[line_leading_spaces] += 1
-#                     else:
-#                         dict_of_spaces[line_leading_spaces] = 1
-
-
-# -------------------------------------------------------------------
-#                 # count the leading spaces in a string
-#                 if len(line) - len(line.lstrip(' ')) < 2:
-#                     tokens = re.findall('\s+', line.lstrip())
-#                     if len(tokens[0]) == 1:
-#                         print(f"{line.find(line.split()[0])} : |{line.rstrip()} : print->{line[line.find(line.split()[0]) + len(line[line.find(line.split()[0]):]):]}")
-#                         # lst_spaces.append(line[line.find(line.split()[0]) + len(line[line.find(line.split()[0]):]):])
-
-#                     elif len(tokens[0]) in dict_of_spaces:
-#                         dict_of_spaces[len(tokens[0])] += 1
-#                     else:
-#                         dict_of_spaces[len(tokens[0])] = 1
-
-# print(lst_spaces)
-# pprint.pprint(dict_of_spaces, width=1)
-
-# print(max(dict_of_spaces.items(), key=operator.itemgetter(1))[0])
-# print(min(dict_of_spaces.items(), key=operator.itemgetter(1))[0])
-
-
-
-
+# TODO:
+"""
+Update the code so it preserves the empty lines,
+instead of removing them 
+"""
