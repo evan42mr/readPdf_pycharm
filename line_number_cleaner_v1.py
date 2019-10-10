36d291f845627b65d7c8e5ba20ab9429b21026b2
Evan <evan@42maru.ai>
2019-10-10 16:37:40 +0900

    Working code for both files with continues and discontinues numbers on lines

--- line_number_cleaner_v1.py
+++ line_number_cleaner_v1.py
@@ -25,9 +25,11 @@ with open(FILE_NAME) as f:
 
                 # Position number of the digit of each line, plus it's length
                 char_num = line.find(line.split()[0]) + len(line.split()[0])
+
                 line_without_number = line[char_num:]
                 # count the leading spaces in a string
                 line_leading_spaces = len(line_without_number) - len(line_without_number.lstrip(' '))
+                # Stopping condition is the first line with a number
                 if line_without_number.strip():
                     #                     print(f"line: {line} spaces = {line_leading_spaces}")
                     break
@@ -36,50 +38,62 @@ with open(FILE_NAME) as f:
 text = ''
 
 with open(FILE_NAME) as f:
-    char_num = 0
+    char_num = 2
     count = 0
     lst_line = []
     for i, line in enumerate(f):
 
-        #         if i > 649:
+        #         if i > 320:
         #             count += 1
         #             lst_line.append(line)
         #         if count == 15:
         #             break
 
+        # Found the end of a page
+        if line[0] == '\x0c':
+            # print(NEW_PAGE)
+            line = line[1:]
+            text += NEW_PAGE
+
         # Check if the line is not empty and first or second characters are digits
         # And line doesn't belong to a table of contents
-        if line.strip() and (line[0].isdigit() or line[1].isdigit()) and line.find('......') == -1:
-            # Found a line with the number at the beginning
-            if line.split()[0].isdigit():
-                # Slice the line so the sentence starts after the number
-                # Discard if the line is empty
+        if line.strip() and (line[0].isdigit() or line[1].isdigit()) \
+                and line.find('......') == -1 \
+                and line.split()[0].isdigit():  # Found a line with the number at the beginning
 
-                # Position number of the digit of each line, plus it's length
+            # Slice the line so the sentence starts after the number
+            # Discard if the line is empty
+
+            # Position number of the digit of each line, plus it's length
+            if line.find(line.split()[0]) + len(line.split()[0]) > 2:
                 char_num = line.find(line.split()[0]) + len(line.split()[0])
-                line_without_number = line[char_num + line_leading_spaces:]
-                # print(line_without_number)
-                text += line_without_number
 
+            line_without_number = line[char_num + line_leading_spaces:]
+            # print(line_without_number)
+            text += line_without_number
         else:
-            # Found the end of a page
-            if line[0] == '\x0c':
-                # print(NEW_PAGE)
-                line = line[1:]
-                text += NEW_PAGE
-                # Find leading spaces in a leading
-            if len(line) - len(line.lstrip(' ')) >= 1:
-                # print(line[line_leading_spaces + char_num:])
+
+            # Find leading spaces of the current string are
+            # larger then leading spaces of the first line, which is
+            # stored in 'line_leading_spaces'
+            #             print(f"curr_line_spaces: {len(line) - len(line.lstrip(' '))}, line_leading_spaces: {line_leading_spaces}, char_num: {char_num}")
+            if len(line) - len(line.lstrip(' ')) >= line_leading_spaces + char_num:
+
+                #                 print(f"line if:{line[line_leading_spaces + char_num:]}")
                 text += line[line_leading_spaces + char_num:]
             else:
-                # print(line)
+                #                 print(f"line else: {line}")
                 text += line
 
-# print(lst_line)
 print(text)
+# print(lst_line)
 
 # TODO:
 """
 Update the code so it preserves the empty lines,
 instead of removing them 
 """
+
+
+
+
